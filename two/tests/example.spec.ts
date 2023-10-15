b8531afd4dd8c25bace7e075d4b661c56ec55006
Alexey Sokolov <alexvyber@gmail.com>
2023-10-15 19:46:10 +0300

    completed 1-10 videos

--- two/tests/example.spec.ts
+++ two/tests/example.spec.ts
@@ -1,18 +1,45 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.beforeEach(async ({page}) => {
+  await page.goto('http://localhost:5173')
+})
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("Right Heading", async ({page} ) => {
+  const heading = page.locator('h1', { hasText: "Vite + React"})
+await expect(heading).toBeVisible()
+})
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+test.describe('Counter', () => {
+  test('should be visible', async ({page}) => {
+    const counterButton = page.locator("#counter")
+    await expect(counterButton).toBeVisible()
+  })
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+
+  test('should increment', async ({page}) => {
+    const counterButton = page.locator("#counter")
+    await expect(counterButton).toHaveText('count is 0')
+    await counterButton.click()
+    await expect(counterButton).toHaveText('count is 1')
+    await counterButton.click()
+    await expect(counterButton).toHaveText('count is 2')
+  })
+
+})
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
